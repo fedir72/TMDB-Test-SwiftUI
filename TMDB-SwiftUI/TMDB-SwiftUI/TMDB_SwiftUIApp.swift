8e165daa208ab91b2609d6f0bd2420985caf9fd3
Ihor Fedii <fiodor312154@gmail.com>
2025-10-03 16:35:13 +0200

    empty project

---
+++ TMDB-SwiftUI/TMDB-SwiftUI/TMDB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_SwiftUIApp.swift
+//  TMDB-SwiftUI
+//
+//  Created by ihor fedii on 03.10.25.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
